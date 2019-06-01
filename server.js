7ad4abeac002bb931700dc4a13e6e5643145d2fc
Harish Joshi <harishjoshi.1998@gmail.com>
2019-06-01 07:59:18 +0530

    Adding login checks in backend & minor bug fixing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -8,7 +9,7 @@ const posts = require("./routes/api/posts");
 const app = express();
 
 //Body parser middleware
-app.use(bodyParser.uslencodes({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //DB Config
