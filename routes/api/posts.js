ed77ee43878b0c13c1e33bf4ceae5a61568f1a84
j98harish <harishjoshi.1998@gmail.com>
2019-06-14 21:28:30 +0530

    Adding functionality for like and unlike a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -73,7 +73,7 @@ router.delete(
   (req, res) => {
     Profile.findOne({ user: req.user.id }).then(profile => {
       Post.findById(req.params.id)
-        .then(port => {
+        .then(post => {
           //Check for post owner
           if (post.user.toString() !== req.user.id) {
             return res
@@ -83,6 +83,70 @@ router.delete(
 
           //Delete
           post.remove().then(() => res.json({ SUccess: "true" }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
+  }
+);
+
+// @route Post api/like/:id
+//@desc Like post
+//@access Private
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.like.filter(like => like.usertoString() === req.user.id)
+              .lenght > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: "User already liked this post" });
+          }
+
+          //Add the user id to likes array
+          post.like.unshift({ user: req.user.id });
+
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
+  }
+);
+
+// @route Post api/unlike/:id
+//@desc Unlike post
+//@access Private
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.like.filter(like => like.usertoString() === req.user.id)
+              .lenght === 0
+          ) {
+            return res
+              .status(400)
+              .json({ notliked: "You have not yet liked this post" });
+          }
+
+          //Get remove index
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indeOf(req.user.id);
+
+          //Splice out of array
+          post.likes.splice(removeIndex, 1);
+
+          //Save
+          post.save().then(post => res.json(post));
         })
         .catch(err => res.status(404).json({ postnotfound: "No post found" }));
     });
