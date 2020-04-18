ab9dd285299a40b80b03f686f21b3ab0b437dbd9
j98harish <harishjoshi.1998@gmail.com>
2020-04-18 23:17:23 +0530

    display education and and delete education

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { withRouter } from "react-router-dom";
 import Moment from "react-moment";
+import { deleteExperience } from "../../actions/profileAction";
 
 class Experience extends Component {
+  onDeleteClick(id) {
+    this.props.deleteExperience(id);
+  }
+
   render() {
-    const Experience = this.props.experience.map((exp) => (
+    const experience = this.props.experience.map((exp) => (
       <tr key={exp._id}>
         <td>{exp.company}</td>
         <td>{exp.title}</td>
         <td>
-          <Moment format="DD/MM/YYYY">{exp.from}</Moment> -{" "}
-          <Moment format="DD/MM/YYYY">{exp.to}</Moment>
+          <Moment format="DD/MM/YYYY">{exp.from}</Moment> -
+          {exp.to === null ? (
+            " Now"
+          ) : (
+            <Moment format="DD/MM/YYYY">{exp.to}</Moment>
+          )}
         </td>
         <td>
-          <button className="btn btn-danger">Delete</button>
+          <button
+            onClick={this.onDeleteClick.bind(this, exp._id)}
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
         </td>
       </tr>
     ));
@@ -28,9 +41,9 @@ class Experience extends Component {
               <th>Company</th>
               <th>Title</th>
               <th>Years</th>
-              <th></th>
+              <th />
             </tr>
-            {Experience}
+            {experience}
           </thead>
         </table>
       </div>
@@ -39,7 +52,7 @@ class Experience extends Component {
 }
 
 Experience.propTypes = {
-  experience: PropTypes.array.isRequired,
+  deleteExperience: PropTypes.func.isRequired,
 };
 
-export default connect(null)(withRouter(Experience));
+export default connect(null, { deleteExperience })(Experience);
